5383361c15db1941c833a1ece22b5289a5f514b3
Anthonee619 <anthonee619+githhub@gmail.com>
2019-11-30 07:14:43 -0800

    First Push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import Navbar from './components/navbar'
+// import Card from './components/card';
+// import Counters from './components/counters';
+import Hand from './components/hand';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {}
+
+  render() {
+    return (
+      <React.Fragment>
+      <StyledHand>
+        <Hand />
+      </StyledHand>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
+
+// const StyledApp = styled.div `
+//   display:grid;
+// `
+
+const StyledHand = styled.div`
+  background: papayawhip;
+  position: fixed;
+  bottom:0;
+  left:0;
+`;
+
+  // state = {
+  //   counters : [
+  //     { id: 1, value: 4},
+  //     { id: 2, value: 0},
+  //     { id: 3, value: 0},
+  //     { id: 4, value: 0}
+  //   ]
+  // };
+  //
+  // handleIncrement = counter => {
+  //   const counters = [...this.state.counters];
+  //   const index = counters.indexOf(counter);
+  //   counters[index] = {...counter};
+  //   counters[index].value++;
+  //   this.setState({ counters });
+  // }
+  //
+  // handleDelete = (counterId) => {
+  //   const counters = this.state.counters.filter(c => c.id !== counterId);
+  //   this.setState({ counters })
+  // }
+  //
+  // handleReset = () => {
+  //   const counters = this.state.counters.map(c => {
+  //     c.value = 0;
+  //     return c;
+  //   });
+  //   this.setState({counters})
+  // }
+  //
+  // render() {
+  //   return (
+  //     <React.Fragment>
+  //     <Navbar
+  //     totalCounters = {this.state.counters.filter(c => c.value > 0).length}
+  //     />
+  //     <main className="Container">
+  //     <Counters
+  //     counters = {this.state.counters}
+  //     onReset= {this.handleReset}
+  //     onDelete= {this.handleDelete}
+  //     onIncrement={this.handleIncrement}/>
+  //     </main>
+  //     </React.Fragment>
+  //   );
+  // }
---
+++ src/classes/ingredient.js
@@ -0,0 +1,8 @@
+class Ingredient {
+  constructor(key, name){
+    this.key = key;
+    this.name = name;
+    this.fileName = name+ '.png';
+  };
+}
+export default Ingredient;
