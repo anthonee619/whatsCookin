90a82483547afeda69f2b956b60d6ad84a46e098
Anthonee619 <anthonee619+githhub@gmail.com>
2019-12-06 05:22:45 -0800

    vote

--- clientSide/src/components/board/vote.jsx
+++ clientSide/src/components/board/vote.jsx
@@ -3,21 +3,14 @@ import Hand from '../hand/hand';
 import styled from 'styled-components';
 
 const Vote = ({ round, players}) => {
-  console.log(players)
+  console.log(Object.keys(players));
   if (round !== 3) {
     return null;
   }
 
   return (
     <div>
-      {Object.keys(players).map(player => (
-        <Hand
-          cards = {players[player].play_hand}
-          key={player}>
-        </Hand>
 
-
-      )
     </div>
   )
 
